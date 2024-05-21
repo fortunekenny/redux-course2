1ca42abfd190cd78b4d3ae1cd3d28212a2de1390
fortunekenny <gbephraim@gmail.com>
2024-05-21 06:56:51 +0100

    update 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import store from "./store";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
+unsubscribe();
+store.dispatch(bugAdded("Bug 1"));
+
+unsubscribe();
+store.dispatch(bugAdded("Bug 2"));
+
+unsubscribe();
+store.dispatch(bugResolved(1));
+
+// store.dispatch(bugRemoved(1));
+
+console.log(store.getState());
