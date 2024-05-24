f9dda6b09d64d57f988ad32ae7786f67922a5b8a
fortunekenny <gbephraim@gmail.com>
2024-05-24 07:17:39 +0100

    update 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*  
 import { bugAdded, bugRemoved, bugResolved } from "./actions";
 import store from "./store";
 
@@ -15,5 +16,22 @@ unsubscribe();
 store.dispatch(bugResolved(1));
 
 // store.dispatch(bugRemoved(1));
+
+console.log(store.getState());
+*/
+
+import configureStore from "./store/configureStore";
+import * as actions from "./store/bugs";
+
+const store = configureStore();
+
+store.subscribe(() => {
+  console.log("store changed!");
+});
+
+store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(actions.bugAdded("Bug 2"));
+store.dispatch(actions.bugAdded("Bug 3"));
+store.dispatch(actions.bugResolved(1));
 
 console.log(store.getState());
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
-
-const store = createStore(reducer);
-
-console.log(store);
-
-export default store;
